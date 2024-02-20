34801203dce2421bdf11dccf3b5d1da9a9c7f792
Gabe Cornejo <gcornejo441@gmail.com>
2024-02-19 22:53:24 -0500

    adding styles

--- src/Views/HomeControl.axaml.cs
+++ src/Views/HomeControl.axaml.cs
@@ -1,13 +1,36 @@
-using Avalonia;
+using System.Reactive;
+using System.Reactive.Disposables;
+
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+
+using AvaloniaNav.ViewModels;
+
+using ReactiveUI;
 
 namespace AvaloniaNav;
 
-public partial class HomeControl : UserControl
+public partial class HomeControl : ReactiveUserControl<HomeControlViewModel>
 {
     public HomeControl()
     {
         InitializeComponent();
+        this.WhenActivated(disposables =>
+        {
+            ViewModel?.SelectAllTextInteraction.RegisterHandler(async interaction =>
+            {
+                LogViewer.SelectAll();
+                interaction.SetOutput(Unit.Default);
+            }).DisposeWith(disposables);
+        });
+
+    }
+
+    private void Control_OnSizeChanged(object sender,SizeChangedEventArgs e)
+    {
+        if (e.HeightChanged)
+        {
+            ScrollOutputViewer.Height = e.NewSize.Height;
+        }
     }
 }
